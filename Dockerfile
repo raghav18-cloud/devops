91ec6c3cc30ed728f86945ce2c2468edd93f57f4
raghav <rag.xtreams@gmail.com>
2021-06-09 02:07:17 +0530

    latest

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:alpine3.7
 COPY . /app
 WORKDIR /app
 RUN pip install flask 
-EXPOSE 5003
-ENTRYPOINT [ "python" ]
+EXPOSE 5005
+ENTRYPOINT [ "python3" ]
 CMD [ "fls.py" ]
 
--- fls.py
+++ fls.py
@@ -1,11 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "welcome Raghav"
+@app.route('/')                         ####@app.route is decorator, basically routing the request..and next return the value for it.
+@app.route('/hello')
+def Helloworld():
+   return "Hello Raghav"
 
+@app.route('/profile/<username>')
+def profile(username):
+   return "hello from Python %s" % username  ## %s specifically is used to perform concatenation of strings together # append a string within a string
 
-if __name__ == "__main__":
-	app.run(host ='0.0.0.0', port = 5003, debug = True)
+if __name__ == '__main__':
+   app.debug = True
+   app.run(host = '0.0.0.0', port = 5005)
 
