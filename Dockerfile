6dc5c20ed194f6935f5b7f64ceba08f0ef8d9f8c
root <root@ip-172-31-48-152.ec2.internal>
2021-06-13 12:08:18 +0000

    latest_commit

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:alpine3.7
 COPY . /app
 WORKDIR /app
 RUN pip install flask 
-EXPOSE 5005
+EXPOSE 5007
 ENTRYPOINT [ "python3" ]
 CMD [ "fls.py" ]
 
--- fls.py
+++ fls.py
@@ -12,5 +12,5 @@ def profile(username):
 
 if __name__ == '__main__':
    app.debug = True
-   app.run(host = '0.0.0.0', port = 5005)
+   app.run(host = '0.0.0.0', port = 5007)
 
